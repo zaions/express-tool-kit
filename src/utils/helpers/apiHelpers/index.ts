80dda37d4b47054b0030adbc7c6a7381848f1ff3
Ahsan Mahmood <aoneahsan@gmail.com>
2024-10-04 12:48:58 +0500

    updated and deployed

--- src/utils/helpers/apiHelpers/index.ts
+++ src/utils/helpers/apiHelpers/index.ts
@@ -1,246 +1,257 @@
 import type { Response } from 'express';
 import {
-	ResponseCodeEnum,
-	ResponseStatusEnum,
-	ztkMessages,
+  ResponseCodeEnum,
+  ResponseStatusEnum,
+  ztkMessages,
 } from 'zaions-tool-kit';
 
 export const sendApiResponse = (
-	res: Response,
-	options?: {
-		success?: boolean;
-		message?: string;
-		data?: unknown;
-		dataList?: unknown;
-		authToken?: unknown;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    success?: boolean;
+    message?: string;
+    data?: unknown;
+    dataList?: unknown;
+    authToken?: unknown;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		success = true,
-		message = ztkMessages.general.success,
-		data = null,
-		dataList = null,
-		authToken = null,
-		errors = null,
-		code = ResponseCodeEnum.success,
-		status = ResponseStatusEnum.success,
-	} = options || {};
-	return   
+  const {
+    success = true,
+    message = ztkMessages.general.success,
+    data = null,
+    dataList = null,
+    authToken = null,
+    errors = null,
+    code = ResponseCodeEnum.success,
+    status = ResponseStatusEnum.success,
+  } = options || {};
+  return res.status(status).json({
+    success,
+    message,
+    result: {
+      data,
+      dataList,
+      authToken,
+    },
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendApiSuccessResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		data?: unknown;
-		dataList?: unknown;
-		authToken?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    data?: unknown;
+    dataList?: unknown;
+    authToken?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.success,
-		data = null,
-		dataList = null,
-		authToken = null,
-		code = ResponseCodeEnum.success,
-		status = ResponseStatusEnum.success,
-	} = options || {};
+  const {
+    message = ztkMessages.general.success,
+    data = null,
+    dataList = null,
+    authToken = null,
+    code = ResponseCodeEnum.success,
+    status = ResponseStatusEnum.success,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: true,
-		message,
-		data,
-		dataList,
-		authToken,
-		errors: null,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: true,
+    message,
+    data,
+    dataList,
+    authToken,
+    errors: null,
+    code,
+    status,
+  });
 };
 
 export const sendApiFailedResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.failed,
-		errors = null,
-		code = ResponseCodeEnum.failed,
-		status = ResponseStatusEnum.failed,
-	} = options || {};
+  const {
+    message = ztkMessages.general.failed,
+    errors = null,
+    code = ResponseCodeEnum.failed,
+    status = ResponseStatusEnum.failed,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendBadRequestResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.badRequest,
-		errors = null,
-		code = ResponseCodeEnum.badRequest,
-		status = ResponseStatusEnum.badRequest,
-	} = options || {};
+  const {
+    message = ztkMessages.general.badRequest,
+    errors = null,
+    code = ResponseCodeEnum.badRequest,
+    status = ResponseStatusEnum.badRequest,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendUnAuthenticatedErrorResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.unAuthenticated,
-		errors = null,
-		code = ResponseCodeEnum.unAuthenticated,
-		status = ResponseStatusEnum.unAuthenticated,
-	} = options || {};
+  const {
+    message = ztkMessages.general.unAuthenticated,
+    errors = null,
+    code = ResponseCodeEnum.unAuthenticated,
+    status = ResponseStatusEnum.unAuthenticated,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendUnAuthorizedErrorResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.unAuthorized,
-		errors = null,
-		code = ResponseCodeEnum.unAuthorized,
-		status = ResponseStatusEnum.unAuthorized,
-	} = options || {};
+  const {
+    message = ztkMessages.general.unAuthorized,
+    errors = null,
+    code = ResponseCodeEnum.unAuthorized,
+    status = ResponseStatusEnum.unAuthorized,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendNotFoundErrorResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.notFound,
-		errors = null,
-		code = ResponseCodeEnum.notFound,
-		status = ResponseStatusEnum.notFound,
-	} = options || {};
+  const {
+    message = ztkMessages.general.notFound,
+    errors = null,
+    code = ResponseCodeEnum.notFound,
+    status = ResponseStatusEnum.notFound,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendTooManyRequestsErrorResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.tooManyRequests,
-		errors = null,
-		code = ResponseCodeEnum.tooManyRequests,
-		status = ResponseStatusEnum.tooManyRequests,
-	} = options || {};
+  const {
+    message = ztkMessages.general.tooManyRequests,
+    errors = null,
+    code = ResponseCodeEnum.tooManyRequests,
+    status = ResponseStatusEnum.tooManyRequests,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
 
 export const sendItemExistsErrorResponse = (
-	res: Response,
-	options?: {
-		message?: string;
-		errors?: unknown;
-		code?: ResponseCodeEnum;
-		status?: ResponseStatusEnum;
-	}
+  res: Response,
+  options?: {
+    message?: string;
+    errors?: unknown;
+    code?: ResponseCodeEnum;
+    status?: ResponseStatusEnum;
+  }
 ): Response => {
-	const {
-		message = ztkMessages.general.itemExists,
-		errors = null,
-		code = ResponseCodeEnum.itemExists,
-		status = ResponseStatusEnum.itemExists,
-	} = options || {};
+  const {
+    message = ztkMessages.general.itemExists,
+    errors = null,
+    code = ResponseCodeEnum.itemExists,
+    status = ResponseStatusEnum.itemExists,
+  } = options || {};
 
-	return sendApiResponse(res, {
-		success: false,
-		message,
-		data: null,
-		errors,
-		code,
-		status,
-	});
+  return sendApiResponse(res, {
+    success: false,
+    message,
+    data: null,
+    errors,
+    code,
+    status,
+  });
 };
