f0c32367bba1930745be0fa72ff10bbbf53ea3d2
Ahsan Mahmood <aoneahsan@gmail.com>
2024-10-12 03:19:50 +0500

    packages updated

--- src/utils/helpers/validationHelpers/index.ts
+++ src/utils/helpers/validationHelpers/index.ts
@@ -1,31 +1,35 @@
 import { Request, Response } from 'express';
-import { ZodObject, ZodError, ZodEffects } from 'zod';
+import { ZodEffects, ZodError, ZodObject } from 'zod';
 import { sendBadRequestResponse } from '..';
 
 export const validateRequestInputData = async <T extends object>({
-	req,
-	res,
-	validationSchema,
+  req,
+  res,
+  validationSchema,
 }: {
-	req: Request;
-	// eslint-disable-next-line
-	validationSchema: ZodObject<any> | ZodEffects<any>;
-	res: Response;
+  req: Request;
+  // eslint-disable-next-line
+  validationSchema: ZodObject<any> | ZodEffects<any>;
+  res: Response;
 }): Promise<T | Response> => {
-	const data = { ...req.body };
+  const data = { ...req.body };
 
-	let validationErrors;
-	try {
-		validationSchema.parse(data);
-	} catch (error) {
-		validationErrors = error as ZodError;
-	}
+  let validationErrors;
+  try {
+    validationSchema.parse(data);
+  } catch (error) {
+    validationErrors = error as ZodError;
+  }
 
-	if (validationErrors?.formErrors?.fieldErrors) {
-		return sendBadRequestResponse(res, {
-			errors: validationErrors?.formErrors?.fieldErrors,
-		});
-	} else {
-		return data satisfies T;
-	}
+  if (validationErrors?.formErrors?.fieldErrors) {
+    return sendBadRequestResponse(res, {
+      message: 'Error occurred in "validateRequestInputData" helper function.',
+      errors: {
+        ...(validationErrors?.formErrors?.fieldErrors ?? {}),
+        formErrors: [...(validationErrors?.formErrors?.formErrors ?? [])],
+      },
+    });
+  } else {
+    return data satisfies T;
+  }
 };
