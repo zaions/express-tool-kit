9e4fd6e3cf15a84711a203621c23986b48a3acd3
Ahsan Mahmood <aoneahsan@gmail.com>
2024-10-11 16:07:06 +0500

    packages updated, and auth interface updated

--- src/types/generic/index.ts
+++ src/types/generic/index.ts
@@ -1,21 +1,24 @@
 import type { Response } from 'express';
 import type { Options } from 'express-rate-limit';
+import { PermissionEnum, RoleEnum } from 'zaions-tool-kit';
 
 export interface IExpressRateLimiterOptions extends Partial<Options> {}
 
 export interface IConfigureZETKOptions {
-	rateLimiterOptions?: IExpressRateLimiterOptions;
+  rateLimiterOptions?: IExpressRateLimiterOptions;
 }
 
 export interface IApiResponse extends Response<any, Record<string, any>> {}
 
 export interface IAuthCheckResultGeneric<A, B> {
-	user: A;
-	userRecord: B;
-	isAdmin: boolean;
-	isPlayer: boolean;
-	isEngager: boolean;
-	userId: string;
-	userEmail: string;
-	userPhoneNumber: string;
+  user: A;
+  userRecord: B;
+  isAdmin: boolean;
+  isPlayer: boolean;
+  isEngager: boolean;
+  userId: string;
+  userEmail: string;
+  userPhoneNumber: string;
+  userRole: RoleEnum;
+  userPermissions: PermissionEnum[];
 }
